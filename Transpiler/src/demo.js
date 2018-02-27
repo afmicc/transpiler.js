aa5d2dd7ab451b9c4064a4145c098d832454d2de
Agustin Martinez Ibarra <afmi.470@gmail.com>
2018-02-27 12:46:37 -0300

    Create transpiler

---
+++ Transpiler/src/demo.js
@@ -0,0 +1,24 @@
+const greetings = ['Welcome', 'Willkommen', 'Hey', 'Gruezi'];
+
+export function next() {
+    let idx = Math.floor(Math.random() * greetings.length)
+    return greetings[idx];
+}
+
+export function nth(n = 0) {
+    return greetings[n];
+}
+
+function resolveAfter2Seconds(x) { 
+    return new Promise(resolve => {
+      setTimeout(() => {
+        resolve(x);
+      }, 2000);
+    });
+  }
+  
+  async function f1() {
+    var x = await resolveAfter2Seconds(10);
+    console.log(x); // 10
+  }
+  f1();
